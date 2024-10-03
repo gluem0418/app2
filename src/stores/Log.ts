90dbbb95be4a730bb07292f913fd9f6a846d7eb2
gluem0418 <145411934+gluem0418@users.noreply.github.com>
2024-10-03 15:32:10 +0900

    Delete src directory

--- src/stores/Log.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia';
-
-// LogStoreの型を定義します。
-export const useLogStore = defineStore('log', {
-  state: () => ({
-    GENERAL: [] as string[],
-    BATTLE: [] as string[],
-  }),
-});
--- src/stores/Position.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia';
-import * as THREE from 'three';
-
-export const usePositionStore = defineStore('position', {
-  state: () => ({
-    // playerPosition: new THREE.Vector3(),
-    playerPosition: new THREE.Vector3(),
-  }),
-});
